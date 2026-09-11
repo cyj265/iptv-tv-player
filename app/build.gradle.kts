102332c80416641b383bb8c66546456e694c08d5
cyj265 <63035285+cyj265@users.noreply.github.com>
2026-09-11 17:09:57 +0800

    v1.7.7 build.gradle.kts: versionCode 28 / 1.7.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.cyj265.iptvplayer"
         minSdk = 21
         targetSdk = 34
-        versionCode = 27
-        versionName = "1.7.6"
+        versionCode = 28
+        versionName = "1.7.7"
     }
 
     buildTypes {
