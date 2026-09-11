5361c02297f8568688721f9efe3e394d4297e746
cyj265 <63035285+cyj265@users.noreply.github.com>
2026-09-11 19:08:30 +0800

    v1.8.5 bump versionCode 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.cyj265.iptvplayer"
         minSdk = 21
         targetSdk = 34
-        versionCode = 35
-        versionName = "1.8.4"
+        versionCode = 36
+        versionName = "1.8.5"
     }
 
     buildTypes {
