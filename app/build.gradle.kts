b6cfa42d1a62f11d8e6398a7f7516c23be08283a
cyj265 <cyj265@users.noreply.github.com>
2026-09-12 20:52:48 +0800

    chore: 版本号 1.14.4 -> 1.14.5 (versionCode 59->60)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.cyj265.iptvplayer"
         minSdk = 23
         targetSdk = 34
-        versionCode = 59
-        versionName = "1.14.4"
+        versionCode = 60
+        versionName = "1.14.5"
     }
 
     buildTypes {
