c73126c98f28e58ee3934214f8fa824d294857a4
cyj265 <cyj265@users.noreply.github.com>
2026-09-12 09:37:07 +0800

    bump v1.10.1 - fix settings panel nav switch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.cyj265.iptvplayer"
         minSdk = 21
         targetSdk = 34
-        versionCode = 45
-        versionName = "1.10.0"
+        versionCode = 46
+        versionName = "1.10.1"
     }
 
     buildTypes {
